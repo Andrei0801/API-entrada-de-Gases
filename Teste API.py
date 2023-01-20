fcb2ca9c19cb82d24b54ab92db3774b2497a769d
Andrei Ballico <106448031+Andrei0801@users.noreply.github.com>
2023-01-20 13:47:42 -0300

    Update Teste API.py
    uma atualização para tentar resolver o problema

--- Teste API.py
+++ Teste API.py
@@ -15,14 +15,14 @@ elapsed = 0
 token_limit = 3600
 
 #URL for login and get API tokens
-url_login = ""
+url_login = "https://api.modulusone.com/login"
 #URL for API telemetry endpoint
-url_telemetry = ""
+url_telemetry = "https://api.modulusone.com/live_telemetry"
 
 #Get API tokens
 def GetAPI_Tokens():
 
-    response_token = requests.post(url_login, json={'login':'', ''})
+    response_token = requests.post(url_login, json={'login':'modulus@randon.com.br', 'password':'nt1675'})
 
     df_token = pd.DataFrame([response_token.json()])
     access_token = df_token['access_token']
@@ -63,18 +63,59 @@ while (True):
         data_dictia = df_live_telemetry[['ia']].to_dict("index")
         data_dictib = df_live_telemetry[['ib']].to_dict("index")
         data_dictdate = df_live_telemetry[['date']].to_dict("index")
-        		
+
+        #DataFreme que será importado
         dataframe = pd.DataFrame({'date': data_dictdate,
                                   'ia': data_dictia,
                                   'ib': data_dictib})
+
+        #importação para tabela comparação e gravar telemetria
+        tabela_dados_compar = pd.read_csv("tabeladadosCompar.csv",delimiter=",")
+        tabela_dados_compar = pd.concat([tabela_dados_compar,dataframe],ignore_index= True)
+        tabela_dados_compar.to_csv("tabeladadosCompar.csv",index=False)
+        
+       
+       
+       
+        #criação de uma função para ler a ultima linha da tabela
+        def get_num_lines(fname):
+            with open(fname) as f:
+                for i, _ in enumerate(f):
+                    pass
+                return i + 1
+        num_lines = get_num_lines("tabeladados.csv")
+
+        #criação de uma função para ler a ultima linha da tabela
+        def get_num_lines(fname):
+            with open(fname) as f:
+                for i, _ in enumerate(f):
+                    pass
+                return i + 1
+        num_lines = get_num_lines("tabeladadosCompar.csv")
+
+        #ler ultima linha da tabela dados
+        tabela_dados2 = pd.read_csv("tabeladados.csv", skiprows=range(1,num_lines))
+
+        #ler ultima linha da tabela dados Compar
+        tabela_dadosCompar2 = pd.read_csv("tabeladadosCompar.csv", skiprows=range(1,num_lines))
+
+        
+        
+        
+        if tabela_dados2 != tabela_dadosCompar2:
+            print("?????????????????")
+
+        #Importar tabela dados e importar telemetria
         tabela_dados = pd.read_csv("tabeladados.csv",delimiter=",")
         tabela_dados = pd.concat([tabela_dados,dataframe],ignore_index= True)
-        
-        
         tabela_dados.to_csv("tabeladados.csv",index=False)
-        print(dataframe)
-        print(tabela_dados)
-            
+        
+        
+        #print(dataframe)
+        #print(tabela_dados)
+        print(tabela_dados2)
+        print(tabela_dadosCompar2)
+        
         
     
 
